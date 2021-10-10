d70439f668ad5ddee9caaad44256dc5b716b1e85
Adwait Datar <datar.adwait@gmail.com>
2021-10-10 23:33:29 +0200

    Adds final reproducible results for LPV examples

--- LPV_Uncertain_system_example/main_LPV_example.m
+++ LPV_Uncertain_system_example/main_LPV_example.m
@@ -15,125 +15,127 @@ tolerences.cvx_tol=1e-3;
 tolerences.bisect_tol=1e-3;
 tolerences.cond_tol=1e8;
 
-% Examples
-% 1. LPV example: Mass with varying kd
-% 2. Uncertain quadrotor with varying quadrotor mass
-example=1;
-switch example
-    case 1
-        % % Mass with friction dynamics
-        kp=[1,1];
-        % Varying parameter: 1: mass, 2: kd
-        varying_param=2;
-        switch varying_param
-            case 1               
-                kd=[1,1];
-                mass=[0.8,1.2];
-            case 2                     
-                kd=[0.8,1.2];
-                mass=[1,1];
-        end
-        L=1:2:30;  % Upper bound on the sector
-        n_L=length(L);
-        dim=2;% spatial dimension (of positions and velocities)
-        addpath(genpath('..\vehicles\LPV_models'))
-        G_veh=define_G_mass_with_friction_LPV_affine(dim,kd,mass,kp);
-        fig_name='LPV_mass_with_varying_kd';
-    case 2
-        % Quadrotor dynamics 
-        addpath(genpath('..\vehicles\quadrotor'))
-        addpath(genpath('..\vehicles\LPV_models'))
-        kp=[1,1];
-        kd=[5,5];
-        mass=[0.2,2];
-        L=1:0.5:10;  % Upper bound on the sector
-        n_L=length(L);
-        dim=2;% spatial dimension (of positions and velocities)
-        % Current implementation only supports dim=2 for quadrotors
-        G_veh=define_G_quad_LPV_wrapped(dim,kp,kd,mass); 
-        fig_name='quad_with_uncertain_mass';
-end
 
-% Multiplier class
-% Select a multiplier class from the following choices
-% 1. Circle criterion
-% 2. Full block circle criterion
-% 3. Zames Falb multipliers
-multiplier_flag=[1,70,71,69,702,703,704,705];
-for i=2:8
-    switch multiplier_flag(1,i)
-    case 1
-%         multiplier_class.id=1;
-%         save_path=['.\data\mult_flag_CC_',num2str(multiplier_flag(1,i))];
-    case 70  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=1;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
-    case 71  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=1;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=1; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_causal_',num2str(multiplier_flag(1,i))];
-    case 69  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=1;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=-1; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_anti_causal_',num2str(multiplier_flag(1,i))];
-    case 702  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=2;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
-    case 703  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=3;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
-    case 704  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=4;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
-    case 705  
-        multiplier_class.id=7;
-        multiplier_class.rho=-1;
-        multiplier_class.psi_order=5;
-        multiplier_class.odd_flag=0;
-        multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
-        save_path=['.\data\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
-    end    
-    alpha_lims=[0,10]; % Initial range for the bisection algorithm
-    [alpha_best]=sweep_L(G_veh,m,L,alpha_lims,tolerences,multiplier_class);
-    if save_data==1 
-        save(save_path); 
+for eg=1:2
+    example=eg;
+    % Examples
+    % 1. LPV example: Mass with varying kd
+    % 2. Uncertain quadrotor with varying quadrotor mass
+    switch example
+        case 1
+            % % Mass with friction dynamics
+            kp=[1,1];
+            % Varying parameter: 1: mass, 2: kd
+            varying_param=2;
+            switch varying_param
+                case 1               
+                    kd=[1,1];
+                    mass=[0.8,1.2];
+                case 2                     
+                    kd=[0.8,1.2];
+                    mass=[1,1];
+            end
+            L=1:2:30;  % Upper bound on the sector
+            n_L=length(L);
+            dim=2;% spatial dimension (of positions and velocities)
+            addpath(genpath('..\vehicles\LPV_models'))
+            G_veh=define_G_mass_with_friction_LPV_affine(dim,kd,mass,kp);
+            fig_name=' LPV mass with varying kd';
+            save_folder='.\data_mass_friction';
+        case 2
+            % Quadrotor dynamics 
+            addpath(genpath('..\vehicles\quadrotor'))
+            addpath(genpath('..\vehicles\LPV_models'))
+            kp=[1,1];
+            kd=[5,5];
+            mass=[0.2,2];
+            L=1:0.5:10;  % Upper bound on the sector
+            n_L=length(L);
+            dim=2;% spatial dimension (of positions and velocities)
+            % Current implementation only supports dim=2 for quadrotors
+            G_veh=define_G_quad_LPV_wrapped(dim,kp,kd,mass); 
+            fig_name=' quad with two modes';
+            save_folder='.\data_quadrotor';
     end
+
+    % Multiplier class
+    % Select a multiplier class from the following choices
+    % 1. Circle criterion
+    % 2. Full block circle criterion
+    % 3. Zames Falb multipliers
+    multiplier_flag=[70,71,69,702,703,704,705];
+    for i=1:7
+        switch multiplier_flag(1,i)        
+        case 70  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=1;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
+        case 71  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=1;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=1; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_causal_',num2str(multiplier_flag(1,i))];
+        case 69  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=1;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=-1; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_anti_causal_',num2str(multiplier_flag(1,i))];
+        case 702  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=2;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
+        case 703  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=3;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
+        case 704  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=4;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];
+        case 705  
+            multiplier_class.id=7;
+            multiplier_class.rho=-1;
+            multiplier_class.psi_order=5;
+            multiplier_class.odd_flag=0;
+            multiplier_class.causal_flag=0; % 1: causal, -1:anti-causal, 0:non-causal
+            save_path=[save_folder,'\mult_flag_non_causal_',num2str(multiplier_flag(1,i))];    
+        end    
+        alpha_lims=[0,10]; % Initial range for the bisection algorithm
+        [alpha_best]=sweep_L(G_veh,m,L,alpha_lims,tolerences,multiplier_class);
+        if save_data==1 
+            save(save_path); 
+        end
+    end
+    % Generate example quadratic fields (Linear feedback) to test conservatism
+    alpha_best=zeros(1,n_L);
+    for i=1:n_L
+        H=[m,0;0,L(1,i)];
+        G_cl1=feedback(G_veh.G1,H,-1);
+        G_cl2=feedback(G_veh.G2,H,-1);
+        alpha1=-1*max(real(pole(G_cl1)));
+        alpha2=-1*max(real(pole(G_cl2)));
+        alpha_best(1,i)=min(alpha1,alpha2);
+    end
+    save([save_folder,'\lb_lin']);
+    %% Plot data
+    plot_data
 end
-% Generate example quadratic fields (Linear feedback) to test conservatism
-alpha_best=zeros(1,n_L);
-for i=1:n_L
-    H=[m,0;0,L(1,i)];
-    G_cl1=feedback(G_veh.G1,H,-1);
-    G_cl2=feedback(G_veh.G2,H,-1);
-    alpha1=-1*max(real(pole(G_cl1)));
-    alpha2=-1*max(real(pole(G_cl2)));
-    alpha_best(1,i)=min(alpha1,alpha2);
-end
-save('.\data\lb_lin');
-%% Plot data
-plot_data
 %% Functions
 % This functions sweeps L and finds the best covergence rate estimate by
 % running a bisection algorithm for each fixed L
--- LPV_Uncertain_system_example/plot_data.m
+++ LPV_Uncertain_system_example/plot_data.m
@@ -1,25 +1,26 @@
-%% Plot results for Example 7 20 from Scherer and Weilands LMI notes
-clear
-clc
-%% Convergence rate estimates for different multiplier
+%% Plot results 
+% save_folder='.\data_mass_friction';
+% fig_name=' LPV mass with varying kd';
+
+% fig_name=' quad with two modes';
+% save_folder='.\data_quadrotor';
+            
+% Convergence rate estimates for different multiplier
 figure()
-%plot_data_perf('.\data\mult_flag_CC_1','ro')
+plot_data_perf([save_folder,'\mult_flag_causal_71'],'g--')
 hold on    
-plot_data_perf('.\data\mult_flag_causal_71','g--')
-plot_data_perf('.\data\mult_flag_anti_causal_69','b-.')
-plot_data_perf('.\data\mult_flag_non_causal_70','c')
-plot_data_perf('.\data\mult_flag_non_causal_702',':')
-plot_data_perf('.\data\mult_flag_non_causal_703',':')
-plot_data_perf('.\data\mult_flag_non_causal_704',':')
-plot_data_perf('.\data\mult_flag_non_causal_705',':')
-plot_data_perf('.\data\lb_lin','k*')
-%legend('CC(P_1=0,P_3=0)','ZF causal (P_1=0)','ZF anti-causal (P_3=0)','ZF','Example fields')
-%legend('ZF causal (P_1=0)','ZF anti-causal (P_3=0)','ZF order 1','ZF order 5','Example fields')
+plot_data_perf([save_folder,'\mult_flag_anti_causal_69'],'b-.')
+plot_data_perf([save_folder,'\mult_flag_non_causal_70'],'c')
+plot_data_perf([save_folder,'\mult_flag_non_causal_702'],':')
+plot_data_perf([save_folder,'\mult_flag_non_causal_703'],':')
+plot_data_perf([save_folder,'\mult_flag_non_causal_704'],':')
+plot_data_perf([save_folder,'\mult_flag_non_causal_705'],':')
+plot_data_perf([save_folder,'\lb_lin'],'k*')
 legend('ZF causal (P_1=0)','ZF anti-causal (P_3=0)','ZF order 1','ZF order 2','ZF order 3','ZF order 4','ZF order 5','Example fields')
 xlabel('L')
 ylabel('\alpha')
 ylim([0,0.5])
-title('Convergence rates(exponents)')
+title(['Convergence rates(exponents)',fig_name])
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function []=plot_data_perf(file,plot_style)
     save_path=['.\',file];
